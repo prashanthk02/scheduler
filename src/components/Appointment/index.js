8d78d1473bf589e6040899eed56cb09c423fc8ff
prashanthk02 <prashanth.k369@gmail.com>
2022-04-20 22:05:07 -0400

    Create show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import "components/Appointment/styles.scss"
 import "components/Appointment/Header"
 import "components/Appointment/Empty"
+import "components/Appointment/Show"
 
 export default function Appointment(props) {
   return(

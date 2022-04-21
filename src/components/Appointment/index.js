567df39cc723acc0e877eb493c5c1d38f0e82915
prashanthk02 <prashanth.k369@gmail.com>
2022-04-20 21:45:52 -0400

    Create header and empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import "components/Appointment/styles.scss"
+import "components/Appointment/Header"
+import "components/Appointment/Empty"
 
 export default function Appointment(props) {
   return(
-    <article className="appointment">
-      {props.time && `Appointment at ${props.time}`}
-    </article>
+    <article className="appointment"></article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,3 +141,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)

19037fc58a6e13a1df9f9159a5db6339649b3d55
prashanthk02 <prashanth.k369@gmail.com>
2022-04-27 20:30:06 -0400

    Refactor tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -10,12 +10,4 @@ describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
-
-  it("does something it is supposed to do", () => {
-    // ...
-  });
-
-  it("does something else it is supposed to do", () => {
-    // ...
-  });
 });
--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -56,40 +56,6 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  xit("calls onSave function when the name and interviewer is defined", () => {
-    const onSave = jest.fn();
-
-    const { getByText,queryByText } = render(
-      <Form
-        interviewers={interviewers}
-        student="Lydia Miller-Jones"
-        interviewer={interviewers[0].id}
-        onSave={onSave} />
-    );
-
-    fireEvent.click(getByText("Save"));
-  
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/please select an interviewer/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  });
-
-  xit("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} interviewer={1} onSave={onSave} />
-    );
-  
-    const input = getByPlaceholderText("Enter Student Name");
-  
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-    fireEvent.click(getByText("Save"));
-  
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  });
-
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
